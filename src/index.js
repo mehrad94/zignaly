45bbae2b7f9375b96e734813a904e4aecafdbca2
Mehrad Farahnak <mehradfarahnak9@gmail.com>
2021-01-19 17:21:48 +0330

    SendSignal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var http = require("http");
-
+const axios = require("axios");
 //create a server object:
 http
   .createServer(function (req, res) {
@@ -8,3 +8,26 @@ http
   })
   .listen(8080); //the server object listens on port 8080
 //ss
+const sendSignal = async () => {
+  const resSendSignal = await axios.post(
+    "https://zignaly.com/api/signals.php",
+    {
+      key: "75e51daf22242349d46f88e633a3e35d",
+      type: "entry",
+      exchange: "binance",
+      exchangeAccountType: "futures",
+      side: "long",
+      market: "QTUMUSDT",
+      positionSizePercentage: "1",
+      leverage: 1,
+      price: "3.395",
+      takeProfitPrice1: "3.405",
+      stopLossPrice: "3.270",
+      orderType: "stop-limit",
+      buyStopPrice: "3.395"
+    }
+  );
+  console.log({ resSendSignal: resSendSignal.status });
+};
+//S
+sendSignal();
